aba168fcac2953c12b28763543afcda3eb476563
lucasferreyra10 <lucasferreyra10@gmail.com>
2025-11-06 19:15:02 -0300

    feat(G1): agregar G1Ej01 - cálculo de hipotenusa de triángulo rectángulo

---
+++ G1Ej01.cpp
@@ -0,0 +1,27 @@
+/*
+Id.Programa: G1Ej01.cpp
+Autor......: Lic. Hugo Cuello
+Fecha......: julio-2013
+Comentario.: Dadas las medidas de los catetos de un triángulo rectángulo,
+             hallar la medida de su hipotenusa.
+Versión modularizada.
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float a, b, c;
+
+    cout << "Calcular hipotenusa con datos de catetos a y b" << endl;
+    cout << "Cateto a: ";
+    cin >> a;
+    cout << "Cateto b: ";
+    cin >> b;
+
+    c = sqrt(pow(a, 2) + pow(b, 2));
+
+    cout << "Hipotenusa: " << c << endl;
+    return 0;
+}
